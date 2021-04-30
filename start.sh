0775b76fd04f61358b54a3ae9f40a023076ced83
147code <82873868+147code@users.noreply.github.com>
2021-04-30 10:10:13 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,19 +6,18 @@ echo " - - - - - - - - - - UPGRADE - - - - - - - - - - "
 sudo apt upgrade
 
 echo " - - - - - - - - - - INSTALL - - - - - - - - - - "
-sudo apt install vim htop tree lolcat figlet omxplayer mutt git 
+sudo apt install vim htop tree lolcat figlet omxplayer mutt git samba 
 
 echo " - - - - - - - - - - .bash_aliases - - - - - - - - - - "
 cd ~/
 wget -L https://raw.githubusercontent.com/147code/rPi/main/.bash_aliases
-echo " - - - - - - - - - - .vimrc - - - - - - - - - - "
-wget -L https://raw.githubusercontent.com/147code/rPi/main/.vimrc
-sudo mkdir -p ~/.vim/undodir 
 cd -
 
 echo " - - - - - - - - - - mnt - - - - - - - - - - "
 sudo mkdir -p /mnt/SanDisk_64_GB
 echo "UUID=440df52a-ce02-4eed-a1f3-3f6ed5779169 /mnt/SanDisk_64_GB ext4 defaults 0 1" | sudo tee -a /etc/fstab
+sudo mkdir -p /mnt/WD_74_GB
+echo "UUID=84bccf1e-7116-4977-823d-054903faa378 /mnt/WD_74_GB ext4 defaults 0 1" | sudo tee -a /etc/fstab
 sudo mount -a
 
 echo " - - - - - - - - - - SSH - - - - - - - - - - "
